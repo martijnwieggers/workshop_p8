35deb9e9d34d538f606370f670e7931dc5649f3c
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2026-02-18 11:45:56 +0100

    eerst opzet met test

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+
+class CalculatorTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
